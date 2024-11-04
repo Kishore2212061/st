14d1083af0019c3075c3f5b84419a6dcbd891aff
Kishore2212061 <165409556+Kishore2212061@users.noreply.github.com>
2024-11-04 19:33:30 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    uniqueValue: { type: Number, required: true },
     fixedValue: { type: Number, required: true },
     trigFunction: { type: String, required: true, enum: ['sin', 'cos', 'tan'] },
     keyValue1: { type: Number, required: true }, 
