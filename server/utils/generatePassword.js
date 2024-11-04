3621af065626c719cd52ee58bbf7823875513f46
Kishore2212061 <165409556+Kishore2212061@users.noreply.github.com>
2024-11-04 16:05:32 +0530

    Update generatePassword.js

--- server/utils/generatePassword.js
+++ server/utils/generatePassword.js
@@ -7,7 +7,7 @@ function generateDynamicPassword(password, uniqueValue, fixedValue, keyValue, cu
         return sum; 
     }, 0);
     
-    const angle = currentHour + parseFloat(keyValue);
+    const angle = parseInt(currentHour) + parseFloat(keyValue);
     console.log(digitSum)
     console.log(angle)
     console.log(keyValue)
