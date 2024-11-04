2e85d60c4cc329c49285dc5abf0d380574b85716
Kishore2212061 <165409556+Kishore2212061@users.noreply.github.com>
2024-11-04 15:30:22 +0530

    Update Login.js

--- client/src/Login.js
+++ client/src/Login.js
@@ -63,6 +63,8 @@ function Login() {
                 password,
                 dynamicPassword: dynamicPassword, 
                 keyValue,
+                fixedValue,
+                trigFunction
             
             });
 
